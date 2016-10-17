d455bb3623f67a660c2d85d65c7a0e29a24b1504
Neil Atkinson <neil@bluespot.io>
2016-10-17 16:38:54 +0100

    Add a DragDataStore constructor function.

---
+++ src/DragDataStore.js
@@ -0,0 +1,4 @@
+// The data that underlies a drag-and-drop operation
+export default function DragDataStore() {
+  // do nothing
+}
---
+++ test/DragdataStore_test.js
@@ -0,0 +1,9 @@
+var expect = require('expect.js')
+
+import DragDataStore from '../src/DragDataStore'
+
+describe('DragDataStore', function() {
+  xit('does something important', function () {
+
+  })
+})
