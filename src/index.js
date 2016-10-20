e14598c4eacaf7c84aff15cfc3b61cd1a406de13
Neil Atkinson <neil@bluespot.io>
2016-10-20 09:41:30 +0100

    Add dragPreview to the body during drag and update its position throughout.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ export default function setupTouchDNDCustomEvents() {
     store: null,
     dataTransfer: null,
     lastDraggedOver: null,
-    draggedItem: null
+    draggedItem: null,
+    clientX: 0,
+    clientY: 0
   }
 
   var touchDnd = window.touchDnd
@@ -17,14 +19,43 @@ export default function setupTouchDNDCustomEvents() {
   document.addEventListener('touchmove', handleTouchMove, true)
 }
 
+function updateDragPreview(store, clientX, clientY) {
+  var top = clientY - store.dragPreviewY
+  var left = clientX - store.dragPreviewX
+  var width = store.dragPreviewWidth
+  var height = store.dragPreviewHeight
+
+  var style = 'position:fixed; top:' + top + 'px; left:' + left + 'px; width:' + width + 'px; height:' +  height + 'px;'
+
+  var previewContainer = document.getElementById('touchDndPreviewContainer')
+  if (previewContainer) {
+    previewContainer.style = style;
+  } else {
+    previewContainer = document.createElement("div")
+    previewContainer.id = 'touchDndPreviewContainer'
+    previewContainer.appendChild(store.dragPreviewElement)
+    previewContainer.style = style;
+    document.body.appendChild(previewContainer)
+  }
+
+  return previewContainer
+}
+
+function removeDragPreview() {
+  var previewContainer = document.getElementById('touchDndPreviewContainer')
+  if (previewContainer) {
+    document.body.removeChild(previewContainer)
+  }
+}
+
 function handleTouchStart (event) {
   if (event.target.hasAttribute("draggable")) {
     event.preventDefault()
 
-    var target = document.elementFromPoint(
-      event.changedTouches[0].clientX,
-      event.changedTouches[0].clientY
-    )
+    touchDnd.clientX = event.changedTouches[0].clientX
+    touchDnd.clientY = event.changedTouches[0].clientY
+
+    var target = document.elementFromPoint(touchDnd.clientX, touchDnd.clientY)
 
     var store = new DragDataStore()
     var dataTransfer = new DataTransfer(store)
@@ -36,6 +67,12 @@ function handleTouchStart (event) {
     store.mode = 'readwrite'
 
     simulateEvent('touchdragstart', event, dataTransfer, target)
+
+    var previewContainer = updateDragPreview(
+      touchDnd.store,
+      touchDnd.clientX,
+      touchDnd.clientY
+    )
   }
 }
 
@@ -43,17 +80,30 @@ function handleTouchMove (event) {
   if (touchDnd.draggedItem) {
     event.preventDefault()
 
+    touchDnd.clientX = event.changedTouches[0].clientX
+    touchDnd.clientY = event.changedTouches[0].clientY
+
     touchDnd.store.mode = 'readwrite'
+
+    var previewContainer = updateDragPreview(
+      touchDnd.store,
+      touchDnd.clientX,
+      touchDnd.clientY
+    )
+
     var dataTransfer = touchDnd.dataTransfer
 
     simulateEvent('touchdrag', event, dataTransfer, touchDnd.draggedItem)
 
     var lastDraggedOver = touchDnd.lastDraggedOver
 
+    // hide dragPreview so we can get the element underneath
+    previewContainer.hidden = true
     var draggedOverTarget = document.elementFromPoint(
-      event.changedTouches[0].clientX,
-      event.changedTouches[0].clientY
+      touchDnd.clientX,
+      touchDnd.clientY
     )
+    previewContainer.hidden = false // show dragPreview again
 
     if (lastDraggedOver !== draggedOverTarget) {
       if (lastDraggedOver) {
@@ -71,10 +121,10 @@ function handleTouchEnd (event) {
   if (touchDnd.draggedItem) {
     event.preventDefault()
 
-    var target = document.elementFromPoint(
-      event.changedTouches[0].clientX,
-      event.changedTouches[0].clientY
-    )
+    touchDnd.clientX = event.changedTouches[0].clientX
+    touchDnd.clientY = event.changedTouches[0].clientY
+
+    var target = document.elementFromPoint(touchDnd.clientX, touchDnd.clientY)
 
     var store = touchDnd.store
     var dataTransfer = touchDnd.dataTransfer
@@ -88,6 +138,8 @@ function handleTouchEnd (event) {
     touchDnd.store = null
     touchDnd.dataTransfer = null
     touchDnd.draggedItem = null
+
+    removeDragPreview()
   }
 }
 
