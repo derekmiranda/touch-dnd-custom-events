f479d23ba933cc0f3b5ed23e876c51db4f8bf4fc
Neil Atkinson <neil@bluespot.io>
2016-10-20 13:40:21 +0100

    Add eslint and fix linting issues.

--- src/DataTransfer.js
+++ src/DataTransfer.js
@@ -1,95 +1,95 @@
-import FileList from './FileList'
-import parseTextUriList from './parseTextUriList'
+import FileList from './FileList';
+import parseTextUriList from './parseTextUriList';
 
 // DataTransfer objects expose the drag data store
 // https://html.spec.whatwg.org/multipage/interaction.html#datatransferitem
 function DataTransfer(store) {
-  this.store = store
-  this.typeTable = {}
-  this.effectAllowed = "uninitialized"
-  this.types = []
-  this.files = new FileList()
+  this.store = store;
+  this.typeTable = {};
+  this.effectAllowed = "uninitialized";
+  this.types = [];
+  this.files = new FileList();
 }
 
 DataTransfer.prototype.setDragImage = function (element, x, y) {
-  if (!this.store) { return }
-  if (this.store.mode !== "readwrite") { return }
+  if (!this.store) { return; }
+  if (this.store.mode !== "readwrite") { return; }
 
-  var preview = element.cloneNode(true)
-  preview.width = element.clientWidth
-  preview.height = element.clientHeight
-  preview.dragPointOffsetX = -x
-  preview.dragPointOffsetY = -y
+  const preview = element.cloneNode(true);
+  preview.width = element.clientWidth;
+  preview.height = element.clientHeight;
+  preview.dragPointOffsetX = -x;
+  preview.dragPointOffsetY = -y;
 
-  this.store.dragPreviewElement = preview
-}
+  this.store.dragPreviewElement = preview;
+};
 
 DataTransfer.prototype.getData = function (format) {
-  if (this.store.mode === "protected") { return "" }
+  if (this.store.mode === "protected") { return ""; }
 
-  format = format.toLowerCase()
+  format = format.toLowerCase();
 
-  var convertToUrl = false
+  let convertToUrl = false;
   if (format === "text") {
-    format = "text/plain"
+    format = "text/plain";
   } else if (format === "url") {
-    format = "text/uri-list"
-    convertToUrl = true
+    format = "text/uri-list";
+    convertToUrl = true;
   }
 
-  if (!(format in this.typeTable)) { return "" }
+  if (!(format in this.typeTable)) { return ""; }
 
-  var result = this.typeTable[format]
+  let result = this.typeTable[format];
   if (convertToUrl) {
-    // set result to the first URL from the list,
-    // if any, or the empty string otherwise. [RFC2483]
-    result = parseTextUriList(result)[0] || ""
+    // set result to the first URL from the list,;
+    // if any, or the empty string otherwise. [RFC2483];
+    result = parseTextUriList(result)[0] || "";
   }
 
-  return result
-}
+  return result;
+};
 
 DataTransfer.prototype.setData = function (format, data) {
-  if (!this.store) { return }
-  if (this.store.mode !== "readwrite") { return }
+  if (!this.store) { return; }
+  if (this.store.mode !== "readwrite") { return; }
 
-  format = format.toLowerCase()
+  format = format.toLowerCase();
 
   if (format === "text") {
-    format = "text/plain"
+    format = "text/plain";
   } else if (format === "url") {
-    format = "text/uri-list"
+    format = "text/uri-list";
   }
 
-  this.typeTable[format] = data
-  this.types = Object.keys(this.typeTable)
-}
+  this.typeTable[format] = data;
+  this.types = Object.keys(this.typeTable);
+};
 
 DataTransfer.prototype.clearData = function (format) {
-  var self = this
+  const self = this;
 
-  if (!this.store) { return }
-  if (this.store.mode !== "readwrite") { return }
+  if (!this.store) { return; }
+  if (this.store.mode !== "readwrite") { return; }
 
   if (typeof format === "undefined") {
-    // Clear all formats (except "Files")
+    // Clear all formats (except "Files");
     this.types
-      .filter(function (type) { return type !== "Files" })
-      .forEach(function (type) { return self.clearData(type) })
+      .filter(function (type) { return type !== "Files"; })
+      .forEach(function (type) { return self.clearData(type); });
 
-    return
+    return;
   }
 
-  format = format.toLowerCase()
+  format = format.toLowerCase();
 
   if (format === "text") {
-    format = "text/plain"
+    format = "text/plain";
   } else if (format === "url") {
-    format = "text/uri-list"
+    format = "text/uri-list";
   }
 
-  delete this.typeTable[format]
-  this.types = Object.keys(this.typeTable)
-}
+  delete this.typeTable[format];
+  this.types = Object.keys(this.typeTable);
+};
 
-export default DataTransfer
+export default DataTransfer;
--- test/DataTransfer_test.js
+++ test/DataTransfer_test.js
@@ -1,255 +1,265 @@
-var expect = require('expect.js')
-
-import DataTransfer from '../src/DataTransfer'
+/* eslint-env mocha */
+import DataTransfer from '../src/DataTransfer';
+import expect from 'expect.js';
 
 describe('DataTransfer', function() {
-  var store
-  var format = "Text"
-  var dataTransfer
+  var format = 'Text';
+  var store = {'mode': 'readwrite'};
+  var dataTransfer = new DataTransfer(store);
 
   beforeEach(function () {
-    // default test values
-    store = { mode: "readwrite" }
-    dataTransfer = new DataTransfer(store)
-  })
+    store = {'mode': 'readwrite'};
+    dataTransfer = new DataTransfer(store);
+  });
 
   describe('#setDragImage', function () {
     var clonedDragImageElement = {
-      clientWidth: 32,
-      clientHeight: 67
-    }
+      'clientHeight': 67,
+      'clientWidth': 32
+    };
 
     var dragImageElement = {
-      cloneNode: function () { return clonedDragImageElement },
-      clientWidth: 32,
-      clientHeight: 67
-    }
+      'clientHeight': 67,
+      'clientWidth': 32,
+      cloneNode() { return clonedDragImageElement; }
+    };
 
-    var x = 10
-    var y = 21
+    var x = 10;
+    var y = 21;
 
     context('when there is NO store', function () {
       it('does NOT store the preview element', function () {
-        dataTransfer = new DataTransfer(undefined)
-        dataTransfer.setDragImage(dragImageElement, x, y)
-        expect(store.dragPreviewElement).to.equal(undefined)
-      })
-    })
+        dataTransfer = new DataTransfer();
+        dataTransfer.setDragImage(dragImageElement, x, y);
+        expect(typeof store.dragPreviewElement).to.equal('undefined');
+      });
+    });
 
     context('when store mode is NOT "readwrite"', function () {
       it('does NOT store the preview element', function () {
-        store = { mode: "notreadwrite" }
-        dataTransfer = new DataTransfer(store)
-        dataTransfer.setDragImage(dragImageElement, x, y)
-        expect(store.dragPreviewElement).to.equal(undefined)
-      })
-    })
+        store = {'mode': 'notreadwrite'};
+        dataTransfer = new DataTransfer(store);
+        dataTransfer.setDragImage(dragImageElement, x, y);
+        expect(typeof store.dragPreviewElement).to.equal('undefined');
+      });
+    });
 
     context('when store mode is "readwrite"', function () {
       it('stores a clone of the preview element', function () {
-        dataTransfer.setDragImage(dragImageElement, x, y)
-        expect(store.dragPreviewElement).to.equal(clonedDragImageElement)
-      })
+        dataTransfer.setDragImage(dragImageElement, x, y);
+        expect(store.dragPreviewElement).to.equal(clonedDragImageElement);
+      });
 
       describe('the cloned dragPreviewElement', function () {
         it("knows its offset from the touch point", function () {
-          dataTransfer.setDragImage(dragImageElement, x, y)
-          expect(clonedDragImageElement.dragPointOffsetX).to.equal(-x)
-          expect(clonedDragImageElement.dragPointOffsetY).to.equal(-y)
-        })
+          dataTransfer.setDragImage(dragImageElement, x, y);
+          expect(clonedDragImageElement.dragPointOffsetX).to.equal(-x);
+          expect(clonedDragImageElement.dragPointOffsetY).to.equal(-y);
+        });
 
         it("knows its dimensions", function () {
-          dataTransfer.setDragImage(dragImageElement, x, y)
-          expect(clonedDragImageElement.width).to.equal(32)
-          expect(clonedDragImageElement.height).to.equal(67)
-        })
-      })
-    })
-  })
+          dataTransfer.setDragImage(dragImageElement, x, y);
+          expect(clonedDragImageElement.width).to.equal(32);
+          expect(clonedDragImageElement.height).to.equal(67);
+        });
+      });
+    });
+  });
 
   describe('#getData', function () {
     context('when store mode is "protected"', function () {
-      store = { mode: "protected" }
 
       it('returns blank string', function () {
-        expect(dataTransfer.getData(format)).to.equal('')
-      })
-    })
+        store = {'mode': 'protected'};
+        expect(dataTransfer.getData(format)).to.equal('');
+      });
+    });
 
     context('when format is unrecognized', function () {
-      format = "not recognized"
-
       it('returns blank string', function () {
-        expect(dataTransfer.getData(format)).to.equal('')
-      })
-    })
+        expect(dataTransfer.getData('not recognized')).to.equal('');
+      });
+    });
 
     context('when given format is text', function () {
-      var format = "Text"
-
       it('returns stored data', function () {
-        dataTransfer.typeTable = {'text/plain': 'some data'}
-        expect(dataTransfer.getData(format)).to.equal('some data')
-      })
-    })
+        dataTransfer.typeTable = {'text/plain': 'some data'};
+        expect(dataTransfer.getData('Text')).to.equal('some data');
+      });
+    });
 
     context('when given format is url', function () {
-      var format = "Url"
-
       it('returns a blank string by default', function () {
-        expect(dataTransfer.getData(format)).to.equal('')
-      })
+        expect(dataTransfer.getData('Url')).to.equal('');
+      });
 
       it('returns the first uncommented line of any stored data', function () {
-        dataTransfer.typeTable = {'text/uri-list': '# commentedout\nsome data\rsome more data\r\nand even more\n'}
-        expect(dataTransfer.getData(format)).to.equal('some data')
-      })
-    })
-  })
+        dataTransfer.typeTable = {
+          'text/uri-list': '# commentedout\nsome data' +
+            '\r;some more data\r\nand even more\n'
+        };
+        expect(dataTransfer.getData('Url')).to.equal('some data');
+      });
+    });
+  });
 
   describe('#setData', function () {
-    var data = 'this data\nis very important'
+    var data = 'this data\nis very important';
 
     context('when there is NO store', function () {
       it('does NOT store the data', function () {
-        dataTransfer = new DataTransfer(undefined)
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.typeTable).to.be.empty()
-      })
-    })
+        dataTransfer = new DataTransfer();
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.typeTable).to.be.empty();
+      });
+    });
 
     context('when store mode is NOT "readwrite"', function () {
       it('does NOT store the data', function () {
-        store = { mode: "notreadwrite" }
-        dataTransfer = new DataTransfer(store)
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.typeTable).to.be.empty()
-      })
-    })
+        store = {'mode': 'notreadwrite'};
+        dataTransfer = new DataTransfer(store);
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.typeTable).to.be.empty();
+      });
+    });
 
     context('when given format is text', function () {
       it('stores the given data under the key "text/plain"', function () {
-        format = "Text"
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.typeTable['text/plain']).to.equal(data)
-      })
+        format = 'Text';
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.typeTable['text/plain']).to.equal(data);
+      });
 
       it('updates the list of stored types', function () {
-        format = "Text"
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.types).to.contain('text/plain')
-      })
-    })
+        format = 'Text';
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.types).to.contain('text/plain');
+      });
+    });
 
     context('when given format is url', function () {
 
       it('stores the given data under the key "text/uri-list"', function () {
-        format = "Url"
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.typeTable['text/uri-list']).to.equal(data)
-      })
+        format = 'Url';
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.typeTable['text/uri-list']).to.equal(data);
+      });
 
       it('updates the list of stored types', function () {
-        format = "Url"
-        dataTransfer.setData(format, data)
-        expect(dataTransfer.types).to.contain('text/uri-list')
-      })
-    })
-  })
+        format = 'Url';
+        dataTransfer.setData(format, data);
+        expect(dataTransfer.types).to.contain('text/uri-list');
+      });
+    });
+  });
 
   describe('#clearData', function () {
     beforeEach(function () {
-      store = { 'mode': 'readwrite'}
-      dataTransfer = new DataTransfer(store)
+      store = {'mode': 'readwrite'};
+      dataTransfer = new DataTransfer(store);
       dataTransfer.typeTable = {
         'Files': 'some file data',
         'text/plain': 'text data',
         'text/uri-list': 'url data'
-      }
-      dataTransfer.types = ['Files', 'text/plain', 'text/uri-list']
-    })
+      };
+      dataTransfer.types = ['Files', 'text/plain', 'text/uri-list'];
+    });
 
     context('when there is NO store', function () {
       it('does NOT clear the data', function () {
-        store = undefined
-        dataTransfer = new DataTransfer(store)
+        dataTransfer = new DataTransfer(null);
         dataTransfer.typeTable = {
           'text/plain': 'text data',
           'text/uri-list': 'url data'
-        }
-        dataTransfer.clearData('Text')
-        expect(dataTransfer.typeTable['text/plain']).to.equal('text data')
-      })
-    })
+        };
+        dataTransfer.clearData('Text');
+        expect(dataTransfer.typeTable['text/plain']).to.equal('text data');
+      });
+    });
 
     context('when store mode is NOT "readwrite"', function () {
       it('does NOT clear the data', function () {
-        store = { 'mode': 'notreadwrite'}
-        dataTransfer = new DataTransfer(store)
+        store = {'mode': 'notreadwrite'};
+        dataTransfer = new DataTransfer(store);
         dataTransfer.typeTable = {
           'text/plain': 'text data',
           'text/uri-list': 'url data'
-        }
-        dataTransfer.clearData('Text')
-        expect(dataTransfer.typeTable['text/plain']).to.equal('text data')
-      })
-    })
+        };
+        dataTransfer.clearData('Text');
+        expect(dataTransfer.typeTable['text/plain']).to.equal('text data');
+      });
+    });
 
     context('when given format is text', function () {
       it('clears data stored under the key "text/plain"', function () {
-        expect(dataTransfer.typeTable['text/plain']).not.to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).not.to.equal(undefined)
-        dataTransfer.clearData("Text")
-        expect(dataTransfer.typeTable['text/plain']).to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).not.to.equal(undefined)
-      })
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .not.to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .not.to.equal('undefined');
+        dataTransfer.clearData('Text');
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .not.to.equal('undefined');
+      });
 
       it('updates the list of stored types', function () {
-        dataTransfer.types = ['text/plain', 'text/uri-list']
-        expect(dataTransfer.types).to.contain('text/plain')
-        dataTransfer.clearData("Text")
-        expect(dataTransfer.types).not.to.contain('text/plain')
-      })
-    })
+        dataTransfer.types = ['text/plain', 'text/uri-list'];
+        expect(dataTransfer.types).to.contain('text/plain');
+        dataTransfer.clearData('Text');
+        expect(dataTransfer.types).not.to.contain('text/plain');
+      });
+    });
 
     context('when given format is url', function () {
       it('clears data stored under the key "text/uri-list"', function () {
-        expect(dataTransfer.typeTable['text/plain']).not.to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).not.to.equal(undefined)
-        dataTransfer.clearData("Url")
-        expect(dataTransfer.typeTable['text/plain']).not.to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).to.equal(undefined)
-      })
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .not.to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .not.to.equal('undefined');
+        dataTransfer.clearData('Url');
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .not.to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .to.equal('undefined');
+      });
 
       it('updates the list of stored types', function () {
-        dataTransfer.types = ['text/plain', 'text/uri-list']
-        expect(dataTransfer.types).to.contain('text/uri-list')
-        dataTransfer.clearData("Url")
-        expect(dataTransfer.types).not.to.contain('text/uri-list')
-      })
-    })
+        dataTransfer.types = ['text/plain', 'text/uri-list'];
+        expect(dataTransfer.types).to.contain('text/uri-list');
+        dataTransfer.clearData('Url');
+        expect(dataTransfer.types).not.to.contain('text/uri-list');
+      });
+    });
 
     context('when given format is "undefined"', function () {
       it('clears data stored under the key "text/uri-list"', function () {
-        expect(dataTransfer.typeTable['text/plain']).not.to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).not.to.equal(undefined)
-        expect(dataTransfer.typeTable['Files']).not.to.equal(undefined)
-        dataTransfer.clearData(undefined)
-        expect(dataTransfer.typeTable['text/plain']).to.equal(undefined)
-        expect(dataTransfer.typeTable['text/uri-list']).to.equal(undefined)
-        expect(dataTransfer.typeTable['Files']).not.to.equal(undefined)
-      })
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .not.to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .not.to.equal('undefined');
+        expect(typeof dataTransfer.typeTable.Files)
+          .not.to.equal('undefined');
+        dataTransfer.clearData();
+        expect(typeof dataTransfer.typeTable['text/plain'])
+          .to.equal('undefined');
+        expect(typeof dataTransfer.typeTable['text/uri-list'])
+          .to.equal('undefined');
+        expect(typeof dataTransfer.typeTable.Files)
+          .not.to.equal('undefined');
+      });
 
       it('updates the list of stored types', function () {
-        expect(dataTransfer.types).to.contain('text/uri-list')
-        expect(dataTransfer.types).to.contain('text/plain')
-        expect(dataTransfer.types).to.contain('Files')
-        dataTransfer.clearData(undefined)
-        expect(dataTransfer.types).not.to.contain('text/uri-list')
-        expect(dataTransfer.types).not.to.contain('text/plain')
-        expect(dataTransfer.types).to.contain('Files')
-      })
-    })
-  })
+        expect(dataTransfer.types).to.contain('text/uri-list');
+        expect(dataTransfer.types).to.contain('text/plain');
+        expect(dataTransfer.types).to.contain('Files');
+        dataTransfer.clearData();
+        expect(dataTransfer.types).not.to.contain('text/uri-list');
+        expect(dataTransfer.types).not.to.contain('text/plain');
+        expect(dataTransfer.types).to.contain('Files');
+      });
+    });
+  });
 
-})
+});
+
